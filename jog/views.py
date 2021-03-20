505ea739d2264212be6f1dd17d24337eff32012e
Ziven <shigaoyu615@gmail.com>
2021-03-20 22:08:58 +1100

    download dataset from api

--- jog/views.py
+++ jog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse,HttpResponseNotFound
+import json
 
 
 # Create your views here.
@@ -14,4 +14,16 @@ def path(request):
     if request.method == "GET":
         lat = request.GET.get('lat',default = 0)
         long = request.GET.get('long',default = 0)
-    return HttpResponse("Your location is "+lat+'  '+long )
+        ifreturn = request.GET.get('ifreturn',default = 'False')
+    response = [{
+        "longitude":144.95963315775296,
+            "latitude":-37.80912284071033},
+                 {
+                     "longitude":144.9825356942,
+                     "latitude":-37.8452163566
+                 }]
+
+    if ifreturn == 'True' or ifreturn == 'False':
+        return HttpResponse(json.dumps(response),content_type='application/json')
+    else:
+        return HttpResponseNotFound('error input')

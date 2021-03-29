d2d6ef31b57c4e5db6f8ddbc4b154fd30b462f9d
Ziven <shigaoyu615@gmail.com>
2021-03-30 01:26:01 +1100

    edit setting static dirs

--- server/settings.py
+++ server/settings.py
@@ -122,5 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATICFILES_DIRS = (
+    os.path.join(os.path.normpath(BASE_DIR), "static")
+)
 STATIC_URL =  os.path.join(BASE_DIR,"static/")

8b19cb8a571981d9b7c18ecbea23bf7ed5e069f9
Ubuntu <ubuntu@ip-172-31-32-81.us-east-2.compute.internal>
2021-03-30 01:30:11 +0000

    update requirements.txt

--- server/settings.py
+++ server/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_DIRS = (
-    os.path.join(os.path.normpath(BASE_DIR), "static")
+    os.path.join(os.path.normpath(BASE_DIR), "static"),
 )
 STATIC_URL =  os.path.join(BASE_DIR,"static/")
